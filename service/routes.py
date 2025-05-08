6044e3797d9f47a3cc661b184ff5bcf1e9c7d7d8
kirans-02 <kiran.sathu@encora.com>
2025-05-08 16:42:01 +0530

    Updated routes.py

--- service/routes.py
+++ service/routes.py
@@ -85,28 +85,36 @@ def create_accounts():
 # UPDATE AN EXISTING ACCOUNT
 ######################################################################
 
-def test_update_account(self):
-    """It should Update an existing Account"""
-    # create an Account to update
-    test_account = AccountFactory()
-    resp = self.client.post(BASE_URL, json=test_account.serialize())
-    self.assertEqual(resp.status_code, status.HTTP_201_CREATED)
-
-    # update the account
-    new_account = resp.get_json()
-    new_account["name"] = "Something Known"
-    resp = self.client.put(f"{BASE_URL}/{new_account['id']}", json=new_account)
-    self.assertEqual(resp.status_code, status.HTTP_200_OK)
-    updated_account = resp.get_json()
-    self.assertEqual(updated_account["name"], "Something Known")
+@app.route("/accounts/<int:account_id>", methods=["PUT"])
+def update_accounts(account_id):
+    """
+    Update an Account
+    This endpoint will update an Account based on the posted data
+    """
+    app.logger.info("Request to update an Account with id: %s", account_id)
+    account = Account.find(account_id)
+    if not account:
+        abort(status.HTTP_404_NOT_FOUND, f"Account with id [{account_id}] could not be found.")
+    account.deserialize(request.get_json())
+    account.update()
+    return account.serialize(), status.HTTP_200_OK    
 
 
 ######################################################################
 # DELETE AN ACCOUNT
 ######################################################################
 
-# ... place you code here to DELETE an account ...
-
+@app.route("/accounts/<int:account_id>", methods=["DELETE"])
+def delete_accounts(account_id):
+    """
+    Delete an Account
+    This endpoint will delete an Account based on the account_id that is requested
+    """
+    app.logger.info("Request to delete an Account with id: %s", account_id)
+    account = Account.find(account_id)
+    if account:
+        account.delete()
+    return "", status.HTTP_204_NO_CONTENT
 
 ######################################################################
 #  U T I L I T Y   F U N C T I O N S
