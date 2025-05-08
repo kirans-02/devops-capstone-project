62f2599f0844a72f961d5c43beb17cb1433a6bf4
kirans-02 <kiran.sathu@encora.com>
2025-05-08 16:34:09 +0530

    Updated routes.py

--- service/routes.py
+++ service/routes.py
@@ -85,7 +85,20 @@ def create_accounts():
 # UPDATE AN EXISTING ACCOUNT
 ######################################################################
 
-# ... place you code here to UPDATE an account ...
+def test_update_account(self):
+    """It should Update an existing Account"""
+    # create an Account to update
+    test_account = AccountFactory()
+    resp = self.client.post(BASE_URL, json=test_account.serialize())
+    self.assertEqual(resp.status_code, status.HTTP_201_CREATED)
+
+    # update the account
+    new_account = resp.get_json()
+    new_account["name"] = "Something Known"
+    resp = self.client.put(f"{BASE_URL}/{new_account['id']}", json=new_account)
+    self.assertEqual(resp.status_code, status.HTTP_200_OK)
+    updated_account = resp.get_json()
+    self.assertEqual(updated_account["name"], "Something Known")
 
 
 ######################################################################
